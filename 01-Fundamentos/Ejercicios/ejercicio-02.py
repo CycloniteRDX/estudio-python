4f011f644ec4051e64ca0df04311d29adff4a30f
CycloniteRDX <219207771+CycloniteRDX@users.noreply.github.com>
2026-09-13 11:37:38 +0200

    fix: inicialización variables y cálculo media

--- 01-Fundamentos/Ejercicios/ejercicio-02.py
+++ 01-Fundamentos/Ejercicios/ejercicio-02.py
@@ -1,20 +1,24 @@
 n_mediciones = int(input('Introduce el número de mediciones: '))
 print('Ahora introduce el valor de las mediciones')
 
-medicion = 0
-min = 0
-max = 0
-media = 0
-suma_medicion = 0
+
+
 for n in range(n_mediciones):
-    medicion = float(input(f'Introduce el valor de la medicion {n+1}: '))
-    suma_medicion += medicion
+    if n == 0: # si estamos en la primera medicion/valor
+        medicion = float(input(f'Introduce el valor de la medicion {n+1}: '))
+        minimo = medicion
+        maximo = medicion
+        suma_medicion = medicion
+    else:
+        medicion = float(input(f'Introduce el valor de la medicion {n+1}: '))
 
-    if medicion > max:
-        max = medicion
-    if medicion < min:
-        min = medicion
-    media = suma_medicion / n_mediciones
+        if medicion > maximo:
+            maximo = medicion
+        if medicion < minimo:
+            minimo = medicion
+        suma_medicion += medicion
 
-print(f'Temperatura media: {media:.2f}\nTemperatura max: {max:.2f}\nTemperatura min: {min:.2f}\nNúmero de mediciones: {n_mediciones}')
+media = suma_medicion / n_mediciones
+
+print(f'Temperatura media: {media:.2f}\nTemperatura max: {maximo:.2f}\nTemperatura min: {minimo:.2f}\nNúmero de mediciones: {n_mediciones}')
 

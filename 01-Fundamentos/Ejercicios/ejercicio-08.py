c1999b01d1df70d8916f7c36909da47ff2a25a1a
CycloniteRDX <219207771+CycloniteRDX@users.noreply.github.com>
2026-09-15 11:06:05 +0200

    mejora: pedir números solo una vez

--- 01-Fundamentos/Ejercicios/ejercicio-08.py
+++ 01-Fundamentos/Ejercicios/ejercicio-08.py
@@ -7,18 +7,15 @@ menu = ("1. Sumar dos números\n"
 opcion = 0
 while opcion != 4:
     opcion = int(input(menu))
-    if opcion == 1:
+    if opcion == 1 or opcion == 2 or opcion == 3:
         numero1 = int(input('Introduce el número 1: '))
         numero2 = int(input('Introduce el número 2: '))
-        print(f'Resultado: {numero1 + numero2}')
-    elif opcion == 2:
-        numero1 = int(input('Introduce el número 1: '))
-        numero2 = int(input('Introduce el número 2: '))
-        print(f'Resultado: {numero1 - numero2}')
-    elif opcion == 3:
-        numero1 = int(input('Introduce el número 1: '))
-        numero2 = int(input('Introduce el número 2: '))
-        print(f'Resultado: {numero1 * numero2}')
+        if opcion == 1:
+            print(f'Resultado: {numero1 + numero2}')
+        elif opcion == 2:
+            print(f'Resultado: {numero1 - numero2}')
+        else:
+            print(f'Resultado: {numero1 * numero2}')
     elif opcion == 4:
         pass
     else:

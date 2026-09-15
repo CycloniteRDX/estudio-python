219e525dd5eab9fa1a5534187da7f4e3c770fc2f
CycloniteRDX <219207771+CycloniteRDX@users.noreply.github.com>
2026-09-15 11:34:53 +0200

    aclaración sobre el bucle for con range

--- 01-Fundamentos/Ejercicios/ejercicio-04.py
+++ 01-Fundamentos/Ejercicios/ejercicio-04.py
@@ -2,7 +2,7 @@ numero = int(input("Ingrese un numero entero positivo: "))
 
 print('Números encontrados: ', end = " ")
 n_divisores = 0
-for i in range(1,numero+1):
+for i in range(1,numero+1): # se suma 1 a número porque empezamos el range en 1, no en 0
     if numero % i == 0:
         print(i, end = " ")
         n_divisores += 1

37bc17282515eef85219ad1e07094536ddfd4cb3
CycloniteRDX <219207771+CycloniteRDX@users.noreply.github.com>
2026-09-15 10:01:46 +0200

    mejora en n_intentos y en blucle

--- 01-Fundamentos/Ejercicios/ejercicio-05.py
+++ 01-Fundamentos/Ejercicios/ejercicio-05.py
@@ -1,9 +1,12 @@
 password = 'python123'
+pass_introducida = ""
 n_intentos = 0
 
-while input('Introduce la contraseña: ') != password:
-    print('Contraseña incorrecta')
+while pass_introducida != password:
+    pass_introducida = input('Introduce la contraseña: ')
     n_intentos += 1
+    if pass_introducida != password:
+        print('Contraseña incorrecta')
 
 print('Acceso concedido')
-print(f'Número de intentos: {n_intentos+1}')
+print(f'Número de intentos: {n_intentos}')

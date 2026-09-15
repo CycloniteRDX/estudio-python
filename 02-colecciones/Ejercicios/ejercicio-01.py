61dfe33d4806d534c0d2bd9ee532d9500ea68dbd
CycloniteRDX <219207771+CycloniteRDX@users.noreply.github.com>
2026-09-15 11:46:34 +0200

    sustitución del último for por función len()

--- 02-colecciones/Ejercicios/ejercicio-01.py
+++ 02-colecciones/Ejercicios/ejercicio-01.py
@@ -1,6 +1,5 @@
 n_mediciones = int(input('Ingrese el número de mediciones: '))
 lista_temperaturas = []
-n_temperaturas = 0
 
 for i in range(n_mediciones):
     temperatura = float(input(f'Temperatura {i+1}: '))
@@ -9,6 +8,4 @@ for i in range(n_mediciones):
 print(lista_temperaturas)
 for valor in lista_temperaturas:
     print(f'Temperatura: {valor} ºC')
-for valor in lista_temperaturas:
-    n_temperaturas += 1
-print(f'Número de temperaturas: {n_temperaturas}')
+print(f'Número de temperaturas: {len(lista_temperaturas)}')
